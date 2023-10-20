628b8dc116e4bf62c43a6bfaf690ae6e0cb81d86
iamAK <akannappan27@gmail.com>
2023-10-20 17:07:40 +0800

    finished sorting products by nearest  distance

--- front-end/vue-project/src/components/PublicListings.vue
+++ front-end/vue-project/src/components/PublicListings.vue
@@ -14,16 +14,16 @@ export default {
       searchData: "",
       matchedProducts: [],
       isLiveSearch: false,
-      categories: ["formals", "casuals", "children", "denims", "traditional", "accessories", "dresses"],
-      selectedCatgories: [],
-      sizes: ["XS", "S", "M", "L", "XL", "XXL"],
-      selectedSizes: [],
-      gender: "",
+      noProducts: false,
+      userLocation: "",
+      productsSortedByLocation: [],
       filterByDistance: false,
+      
     };
   },
   created() {
     // Fetch all products
+    this.getUserLocation();
     axios.get("http://localhost:3000/api/v1/listings")
       .then((res) => {
         for(let product of res.data.listedProducts){
@@ -34,6 +34,9 @@ export default {
                 this.allProductsExcludingUserProducts.push(product);
             }
             this.allProducts.push(product);
+        }
+        if(this.allProducts.length == 0){
+            this.noProducts = true;
         }
       });
   },
@@ -69,20 +72,13 @@ export default {
         }
         return false;
     },
-    handleFilter(){
-        console.log(this.selectedCatgories)
-        console.log(this.selectedSizes)
-        console.log(this.gender)
-        console.log(this.filterByDistance)
-        this.selectedCatgories = [];
-        this.selectedSizes = [];
-        this.gender = "";
-        this.filterByDistance = false;
-    },
+  
     performLiveSearch(){
         if(!this.searchData){
             this.isLiveSearch = false;
             this.matchedProducts = [];
+            this.noProducts = false;
+            this.sortOnLocation();
         }else{
             this.isLiveSearch = true;
             this.matchedProducts = [];
@@ -92,6 +88,10 @@ export default {
                     this.matchedProducts.push(product);
                 }
             }
+            if(this.matchedProducts.length === 0){
+                this.noProducts = true;
+            }
+            this.sortOnLocation();
         }
        
     },
@@ -100,6 +100,50 @@ export default {
         const searchWords = searchQuery.split(' ');
         // Check if all search words are present in the details
         return searchWords.every(word => details.includes(word));
+    },
+    sortOnLocation(){
+        if(this.filterByDistance){
+            const destinations = [];
+            const productArr = [];
+
+            var arrayofProdutsToSort;
+            if(this.isLiveSearch){
+                arrayofProdutsToSort = this.matchedProducts;
+            }else{
+                arrayofProdutsToSort = this.allProducts
+            }
+            for(let product of arrayofProdutsToSort){
+                if(!this.isProductInCurrentUserProducts(product)){
+                    destinations.push(product.address)
+                    productArr.push(product);
+                }   
+            }
+            const destinationString = destinations.join("|");
+            axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${this.userLocation.latitude},${this.userLocation.longitude}&destinations=${destinationString}&key=AIzaSyAr_5GqoyEDg5MldRwiEBsVwn_6vy-Wooc`)
+            .then((res) => {
+                const results = res.data.rows[0].elements;
+                for(let i=0; i < results.length; i++){
+                    productArr[i]["distanceFromUser"] = results[i].distance.text;
+                }
+                productArr.sort((product, product1) => {
+                    return parseFloat(product.distanceFromUser) - parseFloat(product1.distanceFromUser)
+                })
+                this.productsSortedByLocation = productArr;
+            })
+        }
+    },
+
+    getUserLocation() {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.userLocation = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                };
+            });
+        }else{
+            console.log("No navigator found in browser");
+        }
     },
 
   },
@@ -113,114 +157,85 @@ export default {
     <div class="container mt-3">
       <h2 class="text-success text-center">All Listings</h2>
 
-    <div class="position-relative my-4">
-        <input v-model="searchData" type="text" name="search" id="search" 
-        class="form-control border border-2 border-info rounded-pill p-2 w-100 d-block" 
-        placeholder="Search for products"
-         @change="performLiveSearch"
-        >
-        <span class="input-group-text bg-transparent border-0 position-absolute end-0 top-0 me-3">
-            <i class="bi bi-search display-7"></i>
-        </span>
-        <div class="accordion mt-2 w-25" id="accordionExample">
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    <i class="bi bi-filter"></i>&nbsp;&nbsp;Filter
-                </button>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    <form @submit.prevent="handleFilter" class="d-flex flex-column">
-                        <div class="form-check d-inline-block mx-2">
-                                <input class="form-check-input" type="checkbox" id="distance" v-model="filterByDistance">
-                                <label class="form-check-label" for="distance">
-                                    Find products near me
-                                </label>
-                        </div>
-                        <div>
-                            <h4>Choose Categories</h4>
-                            <div v-for="category in categories" class="form-check d-inline-block mx-2" :key="category">
-                                <label class="form-check-label" :for="category">
-                                    {{ category }}
-                                </label>
-                                <input class="form-check-input" type="checkbox" :value="category" :id="category" v-model="selectedCatgories">
-                            </div>
-                        </div>
-                        <div>
-                            <h4>Choose Size</h4>
-                            <div v-for="size in sizes" class="form-check d-inline-block mx-2" :key="size">
-                                <label class="form-check-label" :for="size">
-                                    {{ size }}
-                                </label>
-                                <input class="form-check-input" type="checkbox" :value="size" :id="size" v-model="selectedSizes">
-                            </div>
-                        </div>
-                        <div>
-                            <h4>Gender</h4>
-                            <div class="form-check d-inline-block mx-2">
-                                <input class="form-check-input" type="radio" id="Male" v-model="gender" value="Male">
-                                <label class="form-check-label" for="Male">
-                                    Male
-                                </label>
-                            </div>
-                            <div class="form-check d-inline-block mx-2">
-                                <input class="form-check-input" type="radio" id="F" v-model="gender" value="Female">
-                                <label class="form-check-label" for="F">
-                                    Female
-                                </label>
-                            </div>
-                            <div class="form-check d-inline-block mx-2">
-                                <input class="form-check-input" type="radio" id="U" v-model="gender" value="Unisex">
-                                <label class="form-check-label" for="U">
-                                    Unisex
-                                </label>
-                            </div>
-                        </div>
-                        
-                        <button class="btn btn-secondary align-self-end">Apply Filters</button>
-                    </form>
-                </div>
-                </div>
-            </div>
+        <div class="position-relative my-4">
+            <input v-model="searchData" type="text" name="search" id="search" 
+            class="form-control border border-2 border-info rounded-pill p-2 w-100 d-block" 
+            placeholder="Search for products"
+            @change="performLiveSearch"
+            >
+            <span class="input-group-text bg-transparent border-0 position-absolute end-0 top-0 me-3">
+                <i class="bi bi-search display-7"></i>
+            </span>
+            
         </div>
-        
-    </div>
 
-    <div class="row mt-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-3" v-if="!isLiveSearch">
-        <div class="col"  v-for="product in paginatedProducts" :key="product._id">
-            <div class="card h-100 position-relative">
-                <span v-if="product.isUserProduct === true" class="badge rounded-pill text-bg-success position-absolute m-2">Your Product!</span>
-                <img :src="product.image" class="card-img-top fixedImgHeight" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Details:</h5>
-                    <p class="card-text">
-                        Size: {{ product.size }} Gender: {{ product.gender }}<br/>
-                        Desecription: <br/>{{ product.description }}<br/>
-                        Address: {{ product.address }}
-                    </p>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" v-model="filterByDistance" @change="sortOnLocation">
+            <label class="form-check-label" for="flexCheckDefault">
+                Sort based on nearest location
+            </label>
+        </div>
+
+        <!-- This will be shown when the user is just viewing all listed products and is not searchinh for anything specific -->
+        <div class="row mt-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-3" v-if="!isLiveSearch && !noProducts && !filterByDistance">
+            <div class="col"  v-for="product in paginatedProducts" :key="product._id">
+                <div class="card h-100 position-relative">
+                    <span v-if="product.isUserProduct === true" class="badge rounded-pill text-bg-success position-absolute m-2">Your Product!</span>
+                    <img :src="product.image" class="card-img-top fixedImgHeight" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Details:</h5>
+                        <p class="card-text">
+                            Size: {{ product.size }} Gender: {{ product.gender }}<br/>
+                            Desecription: <br/>{{ product.description }}<br/>
+                            Address: {{ product.address }}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row mt-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-3" v-else>
-        <div class="col"  v-for="product in matchedProducts" :key="product._id">
-            <div class="card h-100 position-relative">
-                <img :src="product.image" class="card-img-top fixedImgHeight" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Details:</h5>
-                    <p class="card-text">
-                        Size: {{ product.size }} Gender: {{ product.gender }}<br/>
-                        Desecription: <br/>{{ product.description }}<br/>
-                        Address: {{ product.address }}
-                    </p>
+
+        <!-- This will be shown when the user is performing live search -->
+        <div class="row mt-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-3" v-if="isLiveSearch && !noProducts && !filterByDistance">
+            <div class="col"  v-for="product in matchedProducts" :key="product._id">
+                <div class="card h-100 position-relative">
+                    <img :src="product.image" class="card-img-top fixedImgHeight" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Details:</h5>
+                        <p class="card-text">
+                            Size: {{ product.size }} Gender: {{ product.gender }}<br/>
+                            Desecription: <br/>{{ product.description }}<br/>
+                            Address: {{ product.address }}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  
+
+        <!-- This will be displayed when filtering by distance is done -->
+        <div class="row mt-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-3" v-if="filterByDistance && !noProducts">
+            <div class="col"  v-for="product in productsSortedByLocation" :key="product._id">
+                <div class="card h-100 position-relative">
+                    <img :src="product.image" class="card-img-top fixedImgHeight" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Details:</h5>
+                        <p class="card-text">
+                            Size: {{ product.size }} Gender: {{ product.gender }}<br/>
+                            Desecription: <br/>{{ product.description }}<br/>
+                            Address: {{ product.address }}
+                            Distance: {{ product.distanceFromUser }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container" v-if="noProducts">
+            <p>No Products found!</p>
+        </div>
+    
       <!-- Bootstrap Pagination -->
-      <nav class="mt-5" v-if="!isLiveSearch">
+      <nav class="mt-5" v-if="!isLiveSearch && !filterByDistance">
         <ul class="pagination justify-content-center">
           <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
             <a class="page-link" href="#" @click="prevPage">Previous</a>

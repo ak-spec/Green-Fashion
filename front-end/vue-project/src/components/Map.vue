ed0214a79c6f7f4f217b2062559f00fc7f9d63ff
GJlonely1 <foo.guojie@gmail.com>
2023-10-26 14:00:36 +0800

    improved map functionality

--- front-end/vue-project/src/components/Map.vue
+++ front-end/vue-project/src/components/Map.vue
@@ -1,71 +1,92 @@
-<!-- <template>
-    <div>
-        <GoogleMap :center="center" :zoom="10" style="width: 100%; height: 555px;">
-            <GoogleMapMarker :key="index" v-for="(gmp, index) in locations" :position="gmp" @click="setPlace(gmp)"> 
-            </GoogleMapMarker>
-        </GoogleMap>
+<template>
+    <div class="personal">
+        <h1>Your Coordinates: </h1>
+        <p> Latitude: {{ coordinates.lat }}</p>
+        <p> Longitude: {{ coordinates.lng }}</p>
     </div>
+    
+    <GoogleMap id="map" v-bind:api-key='apiKey' style="width: 100%; height: 500px" :center="coordinates" :zoom="8">
+        <CustomMarker :options="{ position: coordinates  }">
+            <div style="text-align: center">
+                <div style="font-size: 1.125rem; color: red; font-weight: bold;">My Location</div>
+                <img src="../assets/images/current_location.png" width="30" height="30" style="margin-top: 8px" />
+            </div>
+        </CustomMarker>
+        <MarkerCluster>
+            <Marker v-for="(location, i) in locations" :options="{ position: location }" :key="i">
+                <InfoWindow v-model="infoWindow">
+                    <div id="content">
+                        This is the infowindow content
+                    </div>
+                </InfoWindow>
+            </Marker>
+        </MarkerCluster>
+        
+    </GoogleMap>
 </template>
 
-
-
 <script>
-    import { ref, watch } from 'vue';
-    import { GoogleMap } from 'vue3-google-map';
+    import {GoogleMap, Marker, MarkerCluster, CustomMarker, InfoWindow } from "vue3-google-map"
+
+
 
 
     export default {
-        name: 'GoogleMap',
+        setup() {
+            const locations = [
+            { lat: 1.36097, lng: 103.84256 }, //Bishan Chinese Corps Salvation Army
+            { lat: 1.29504, lng: 103.8160599 }, // The Salvation Army Tanglin Rd
+            { lat: 1.29039, lng: 103.77584},  // The Salvation Army, The Haven
+            { lat: 1.3624072477201,  lng: 103.76777425712}, //Salvation Army Upper Bukit Timah
+            { lat: 1.350071, lng: 103.970425 }, //Salvation Army Upper Changi
+            { lat: 1.31953, lng: 103.8539}, //The Salvation Army, Balestier Corps
+            { lat: 1.2806075420456, lng: 103.82620833256}, //The Salvation Army, Bukit Merah
+            // { lat: , lng: },
+        ];
+            const infoWindow = ref(true)
+
+        return {locations, infoWindow};
+        },
+    
         data() {
             return {
-                center : {
+                apiKey : "AIzaSyDqWE6EI_LPusVBX3PdGeNNW1PMhT4Zeb0", 
+                coordinates : {
                     lat: 1.290270,
 		            lng: 103.851959,
-                },
-                locations : [],
-                currentLocation : null,
-            };
-        },
-
-        mounted() {
-            this.setLocationLatLng();
-        },
-
-        methods: {
-            setPlace(loc) {
-                this.currentLocation = loc;
-            },
-            setLocationLatLng: function() {
-                navigator.geolocation.getCurrentPosition(geolocation => {
-                this.center = {
-                lat: geolocation.coords.latitude,
-                lng: geolocation.coords.longitude
-          };
-        });
-            this.locations = [
-            {
-                lat: 39.7837304,
-                lng: -100.4458825,
-                label: 'United States'
-            },
-            {
-                lat: 38.6529545,
-                lng: -90.2411166,
-                label: 'St. Louis'
-            },
-            {
-                lat: 41.3828939,
-                lng: 2.1774322,
-                label: 'Barcelona'
-            },
-            {
-                lat: -10.3333333,
-                lng: -53.2,
-                label: 'Brazil'
+                }
             }
-        ];
+        },
+        created() {
+            this.$getLocation() 
+            .then ((coordinates) => {
+                this.coordinates.lat = coordinates.lat
+                this.coordinates.lng = coordinates.lng
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        },
+
+        components : {
+            GoogleMap, Marker, CustomMarker, MarkerCluster, InfoWindow
+        },
+        watch: {
+            infowindow(v) {
+            alert('infowindow has been ' + (v ? 'opened' : 'closed'))
+            }  
         }
     }
+</script>
+
+
+<style>
+.personal {
+    background: linear-gradient(to right, #96705B,#BA9A8E);;
 }
 
-</script> -->
+#map {
+    margin: auto;
+    
+}
+</style>
--- front-end/vue-project/src/main.js
+++ front-end/vue-project/src/main.js
@@ -5,11 +5,16 @@ import "axios";
 
 
 import { createApp } from 'vue'
+// import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vue3GeoLocation from 'vue3-geolocation'
+
+
 
 
 const app = createApp(App)
 app.use(router)
-
+app.use (Vue3GeoLocation)
 app.mount('#app')
+

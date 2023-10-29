a980fc333aa3690e7e0b0ada89c8f14fa74f880f
Rohith-codegod <rohitht.2022@scis.smu.edu.sg>
2023-10-29 12:08:55 +0800

    updated maps to view direction

--- front-end/vue-project/src/components/Map.vue
+++ front-end/vue-project/src/components/Map.vue
@@ -1,19 +1,26 @@
-<template>
-    <div class="personal">
-        <h1>Your Coordinates: </h1>
-        <p> Latitude: {{ coordinates.lat }}</p>
-        <p> Longitude: {{ coordinates.lng }}</p>
+<template class="entirebody">
+    <div v-if="!directionsInfoVisible" class="directions-info">
+        <strong>Find the most closest salvation army now!</strong><br>
+        <strong>simply click on any marker</strong>
+    </div>
+    <div v-if="directionsInfoVisible" class="directions-info">
+        <strong>Your Address:</strong>{{ userAddress }}<br>
+        <strong>Destination Address:</strong> {{ destinationAddress }}<br>
+        <strong>Distance:</strong> {{ distance }}<br>
+        <strong>Duration:</strong> {{ duration }}<br>
+        <p v-if="directions">Directions:</p>
+        <pre v-if="directions" v-for="(step, index) in directions.steps" :key="index">{{ step.html_instructions }}</pre>
     </div>
     
-    <GoogleMap id="map" v-bind:api-key='apiKey' style="width: 100%; height: 500px" :center="coordinates" :zoom="8">
-        <CustomMarker :options="{ position: coordinates  }">
+    <GoogleMap id="map" v-bind:api-key='apiKey' style="width: 1000px; height: 550px;margin:0" :center="coordinates" :zoom="8">
+        <CustomMarker class="mymarker" :options="{ position: coordinates  }">
             <div style="text-align: center">
                 <div style="font-size: 1.125rem; color: red; font-weight: bold;">My Location</div>
                 <img src="../assets/images/current_location.png" width="30" height="30" style="margin-top: 8px" />
             </div>
         </CustomMarker>
         <MarkerCluster>
-            <Marker v-for="(location, i) in locations" :options="{ position: location }" :key="i">
+            <Marker class="anomarker" v-for="(location, i) in locations" :options="{ position: location }" :key="i" @click="showDirections(location, coordinates)">
                 <InfoWindow v-model="infoWindow">
                     <div id="content">
                         This is the infowindow content
@@ -21,15 +28,13 @@
                 </InfoWindow>
             </Marker>
         </MarkerCluster>
-        
     </GoogleMap>
 </template>
 
 <script>
-    import {GoogleMap, Marker, MarkerCluster, CustomMarker, InfoWindow } from "vue3-google-map"
-
-
-
+    import {GoogleMap, Marker, MarkerCluster, CustomMarker, InfoWindow } from "vue3-google-map";
+    import {ref} from 'vue';
+    import axios from 'axios';
 
     export default {
         setup() {
@@ -44,17 +49,61 @@
             // { lat: , lng: },
         ];
             const infoWindow = ref(true)
+            const userAddress = ref('');
+            const destinationAddress = ref('');
+            const distance = ref('');
+            const duration = ref('');
+            const directionsInfoVisible = ref(false);
+            const directions = ref(null);
 
-        return {locations, infoWindow};
-        },
+            const showDirections = (destination, origin) => {
+      // Make an Axios request to the Directions API
+      axios
+        .get('https://maps.googleapis.com/maps/api/directions/json', {
+          params: {
+            origin: `${origin.lat},${origin.lng}`,
+            destination: `${destination.lat},${destination.lng}`,
+            key: "AIzaSyAMPEA8sJ9zhXSrzcwB3Z6dc5a2y2ysILk",
+          },
+        })
+        .then((response) => {
+          // Handle the response to display directions
+          console.log(response.data);
+          const route = response.data.routes[0].legs[0];
+          userAddress.value = route.start_address;
+          destinationAddress.value = route.end_address;
+          distance.value = route.distance.text;
+          duration.value = route.duration.text;
+          directions.value = route;
+          directionsInfoVisible.value = true;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    return {
+      locations,
+      infoWindow,
+      showDirections,
+      userAddress,
+      destinationAddress,
+      distance,
+      duration,
+      directionsInfoVisible,
+      directions,
+    };
+  },
     
         data() {
             return {
-                apiKey : "AIzaSyDqWE6EI_LPusVBX3PdGeNNW1PMhT4Zeb0", 
+                apiKey : "AIzaSyAMPEA8sJ9zhXSrzcwB3Z6dc5a2y2ysILk", 
                 coordinates : {
                     lat: 1.290270,
 		            lng: 103.851959,
-                }
+                },
+
+
             }
         },
         created() {
@@ -81,12 +130,35 @@
 
 
 <style>
-.personal {
+/* .personal {
     background: linear-gradient(to right, #96705B,#BA9A8E);;
-}
+} */
 
 #map {
     margin: auto;
-    
+}
+
+.mymarker{
+    position:relative;
+    animation: jump 1s infinite;
+    animation-delay: 3s;
+}
+
+@keyframes jump{
+    0%
+    {
+        transform:translateY(0)
+    }
+    20%{
+        transform:translateY(-20px)
+    }
+}
+
+.directions-info {
+  background-color: yellow;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  z-index: 1;
 }
 </style>
